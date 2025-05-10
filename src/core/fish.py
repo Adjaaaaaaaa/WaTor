7c208aa24985d0b5a4c29ec25a0bcfe60c59dc6b
Adjaaaaaaaa <adja.hamida@gmail.com>
2025-05-10 20:28:35 +0200

    fix: reset

--- src/core/fish.py
+++ src/core/fish.py
@@ -1,23 +1,3 @@
 
-class Fish:
-    """ 
-    Create an abstract class Fish to represent a living entity in the ecosystem 
-    (tuna or shark), with common methods.
-      """
-    # donne à l'entité créeé une position dans la grille
-    def __init__(self, x, y):
-        self.age = 0
-        self.x = x
-        self.y = y
-        self.neighbors = []
-        self.last_x = None
-        self.last_y = None
         
-
-    # retourne les déplacements possibles:
-    # les directions possibles par rapport à l'entité:  droit, gauche, haut et bas (0,1), (0,-1), (-1,0),(1,0)
-
-    def get_neighbors(self):
-        self.neighbors = [(self.x,self.y+1), (self.x,self.y-1), (self.x-1,self.y),(self.x+1,self.y)]
-        return self.neighbors
 
