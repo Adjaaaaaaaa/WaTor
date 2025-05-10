c5728c7d2ef05963ed4eb90b8048ac71200bc8fd
Adjaaaaaaaa <adja.hamida@gmail.com>
2025-05-10 21:47:46 +0200

    feat: création de la class tuna

---
+++ src/core/tuna.py
@@ -0,0 +1,9 @@
+from .fish import Fish
+from .config import TUNA_REPRODUCTION_TIME
+
+class Tuna(Fish):
+    """ 
+    creation of the prey class
+    """
+    def __init__(self):
+        super().__init__() 
