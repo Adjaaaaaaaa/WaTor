ba3ab692cb0eed254de72cbc1a3d3f40131e4caa
Adjaaaaaaaa <adja.hamida@gmail.com>
2025-05-10 21:00:24 +0200

    feat: création de la class planet

---
+++ src/core/planet.py
@@ -0,0 +1,15 @@
+from ..core.config import WIDTH, HEIGHT
+from ..core.fish import Fish
+class Planet:
+    #création d'une grille vide
+    def __init__(self):
+        self.grid = [[None for _ in range (WIDTH)] for _ in range (HEIGHT)]
+    
+    # placer aléatoirement les poissons dans la gille
+
+    def populate():
+
+
+    
+
+    
